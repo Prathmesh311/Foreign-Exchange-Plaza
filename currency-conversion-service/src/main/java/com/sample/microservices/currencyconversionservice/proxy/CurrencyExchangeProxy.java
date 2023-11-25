59d6622d497a309e4c4e2fb9bffdaf7c54dcc13a
Prathmesh311 <prathmesh.bhondave2@gmail.com>
2023-11-24 19:14:44 -0800

    api gateway added

--- currency-conversion-service/src/main/java/com/sample/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/sample/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.sample.microservices.currencyconversionservice.entity.CurrencyConversion;
 
-@FeignClient(name="currency-exchange", url="http://localhost:8000")
+//@FeignClient(name="currency-exchange", url="http://localhost:8000")
+@FeignClient(name="currency-exchange")
 public interface CurrencyExchangeProxy {
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
